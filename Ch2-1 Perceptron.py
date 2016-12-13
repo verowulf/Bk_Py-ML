1956b6aaa45a4b7e985c06c49765c3ae23fc45b3
verowulf <vati27@gmail.com>
2016-12-13 13:39:09 +0900

    Create Ch2-1 Perceptron.py

---
+++ Ch2-1 Perceptron.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class Perceptron(object):
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        self.w_ = np.zeros(X.shape[1] + 1)
+        self.n_errors_ = []
+
+        for _ in range(self.n_iter):
+            n_errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0]  += update
+                if update != 0.0:
+                    n_errors += 1
+#                n_errors += int(update != 0.0)
+            self.n_errors_.append(n_errors)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
